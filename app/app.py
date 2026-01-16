212e4b80724940ec05d8b338d7674152549bf671
acqarai-tech <acqarai@gmail.com>
2026-01-16 16:43:31 +0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -35,7 +35,9 @@ app.add_middleware(
 allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://truvalu-backend-1.onrender.com",
     ],
+
     
     allow_credentials=True,
     allow_methods=["*"],
