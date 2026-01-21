cf15d04dc2118f7b4144c5ba51e25fc92e82d043
acqarai-tech <acqarai@gmail.com>
2026-01-21 20:25:07 +0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -36,6 +36,7 @@ allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "https://truvalu-backend-1.onrender.com",
+        "https://acqar-mvp.onrender.com"
     ],
 
     
