18af1fa7b405f12ef033e9837f58c096e07568ba
acqarai-tech <acqarai@gmail.com>
2026-04-15 18:09:56 +0500

    Update index.js

--- avm-frontend/src/index.js
+++ avm-frontend/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { loadStripe } from "@stripe/stripe-js";
-loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
+import { loadStripe } from "@stripe/stripe-js";
+
+loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
