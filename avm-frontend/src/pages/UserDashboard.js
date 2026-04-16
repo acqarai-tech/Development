00ed847c38522c2f63cdcdd198406804ba2ebe5b
acqarai-tech <acqarai@gmail.com>
2026-04-16 17:29:56 +0500

    Update UserDashboard.js

--- avm-frontend/src/pages/UserDashboard.js
+++ avm-frontend/src/pages/UserDashboard.js
@@ -4564,7 +4564,7 @@ useEffect(() => {
                 </p>
                <div style={{
   display: 'flex',
-  gap: 24,
+  gap: 50,
   flexWrap: 'wrap',
   flexDirection: window.innerWidth <= 640 ? 'column' : 'row',
 }}>
