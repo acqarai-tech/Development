56b049d80c197629b889214e37666b84f773de3c
acqarai-tech <acqarai@gmail.com>
2026-04-29 19:27:42 +0500

    Update index.js

--- avm-frontend/src/index.js
+++ avm-frontend/src/index.js
@@ -1,10 +1,38 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import "./styles/global.css";
+// import { loadStripe } from "@stripe/stripe-js";
+
+// loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<App />);
+
+
+
+
+
+
+
+
+
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
 import { loadStripe } from "@stripe/stripe-js";
+import { PostHogProvider } from "posthog-js/react";
 
 loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <PostHogProvider
+    apiKey={process.env.REACT_APP_POSTHOG_KEY}
+    options={{ api_host: "https://app.posthog.com" }}
+  >
+    <App />
+  </PostHogProvider>
+);
