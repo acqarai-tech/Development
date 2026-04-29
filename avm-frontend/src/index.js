4ea93c81aee4fca77b37453ffd93114f381bd7bb
acqarai-tech <acqarai@gmail.com>
2026-04-29 19:48:06 +0500

    Update index.js

--- avm-frontend/src/index.js
+++ avm-frontend/src/index.js
@@ -18,6 +18,36 @@
 
 
 
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import "./styles/global.css";
+// import { loadStripe } from "@stripe/stripe-js";
+// import { PostHogProvider } from "posthog-js/react";
+
+// loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <PostHogProvider
+//     apiKey={process.env.REACT_APP_POSTHOG_KEY}
+//     options={{ api_host: "https://app.posthog.com" }}
+//   >
+//     <App />
+//   </PostHogProvider>
+// );
+
+
+
+
+
+
+
+
+
+
+
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -30,7 +60,7 @@ loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <PostHogProvider
-    apiKey={process.env.REACT_APP_POSTHOG_KEY}
+    apiKey="phc_tsbYV5ppfytmT3d5Ueya96nLQfYPhFEJ2tEcqCyokTKY"  // 👈 paste your real key here
     options={{ api_host: "https://app.posthog.com" }}
   >
     <App />
