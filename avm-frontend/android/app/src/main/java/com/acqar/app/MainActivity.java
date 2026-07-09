70a3a54bb874db62551654f518ff8e54a5d7d265
acqarai-tech <acqarai@gmail.com>
2026-07-09 16:42:16 +0500

    Add Capacitor android and ios for mobile app

---
+++ avm-frontend/android/app/src/main/java/com/acqar/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.acqar.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ avm-frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.acqar.app',
+  appName: 'ACQAR',
+  webDir: 'build'
+};
+
+export default config;
