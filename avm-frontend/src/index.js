98e54b7adc4cb9caddb78b30145c4cabb9f4b9e3
acqarai-tech <acqarai@gmail.com>
2026-07-22 19:10:39 +0500

    Update index.js

--- avm-frontend/src/index.js
+++ avm-frontend/src/index.js
@@ -20,7 +20,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/global.css";
+
 import { loadStripe } from "@stripe/stripe-js";
 import posthog from "posthog-js";
 import './index.css'
